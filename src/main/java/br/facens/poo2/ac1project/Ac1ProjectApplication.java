dc56a109ba4790fcd2b798c6b2c4dde3a57d61d3
vaaaarlos <kadu.cerq@gmail.com>
2021-03-24 11:21:26 -0300

    Initial commit

---
+++ src/main/java/br/facens/poo2/ac1project/Ac1ProjectApplication.java
@@ -0,0 +1,13 @@
+package br.facens.poo2.ac1project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac1ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac1ProjectApplication.class, args);
+	}
+
+}
