d06e06be3e64646eb995bf47628d1646d8bbf445
vaaaarlos <kadu.cerq@gmail.com>
2021-03-25 13:07:35 -0300

    Created controller structure

---
+++ src/main/java/br/facens/poo2/ac1project/controller/EventController.java
@@ -0,0 +1,8 @@
+package br.facens.poo2.ac1project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/events")
+public class EventController { }
