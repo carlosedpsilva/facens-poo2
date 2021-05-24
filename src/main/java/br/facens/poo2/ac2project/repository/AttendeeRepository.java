b26561749a72eec869ba0f0b493e0182b63d921a
vaaaarlos <kadu.cerq@gmail.com>
2021-05-23 21:18:23 -0300

    fix: :bug: Correct pageAll Attendees query

--- src/main/java/br/facens/poo2/ac2project/repository/AttendeeRepository.java
+++ src/main/java/br/facens/poo2/ac2project/repository/AttendeeRepository.java
@@ -13,7 +13,7 @@ import br.facens.poo2.ac2project.entity.Attendee;
 public interface AttendeeRepository extends JpaRepository<Attendee, Long> {
 
   @Query("SELECT e FROM Attendee e WHERE "
-      + "((e.balance >= :#{#req.phoneNumber}) OR :#{#req.balance} IS NULL) "
+      + "((e.balance >= :#{#req.balance}) OR :#{#req.balance} IS NULL) "
       + "AND (LOWER(e.name)  LIKE LOWER(CONCAT('%',  CAST(:#{#req.name} AS string), '%')) OR :#{#req.name}  IS NULL) "
       + "AND (LOWER(e.email) LIKE LOWER(CONCAT('%', CAST(:#{#req.email} AS string), '%')) OR :#{#req.email} IS NULL) ")
   Page<Attendee> pageAll(Pageable pageRequest, @Param("req") Attendee entitysSearchFilter);
